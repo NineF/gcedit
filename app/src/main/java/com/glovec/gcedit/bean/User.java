c565587cb1a10c58fed84fedaf4049404bff16e7
NineF <894906165@qq.com>
2017-04-17 22:37:14 +0800

    Rejected commit: Default

--- app/src/main/java/com/glovec/gcedit/bean/User.java
+++ app/src/main/java/com/glovec/gcedit/bean/User.java
@@ -14,6 +14,7 @@ public class User {
         this.username = username;
         this.userpassword = userpassword;
     }
+    public User(){}
 
     public int getUserId() {
         return userId;
